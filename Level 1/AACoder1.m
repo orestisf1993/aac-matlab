0394dd55cab9ed6255ad1670b083be4f789233da
orestisf <orestisf1993@gmail.com>
2016-12-19 23:55:37 +0200

    Improve AACoder1.m comments

--- Level 1/AACoder1.m
+++ Level 1/AACoder1.m
@@ -2,22 +2,26 @@ function AACSeq1 = AACoder1(fNameIn)
 %AACODER1 AAC encoder for Level 1.
 %   AACSEQ1 = AACODER1(FNAMEIN) will perform the AAC encoding for file with
 %   filename FNAMEIN. AACSEQ1 is a struct that has the following elements:
-%   - frameType
-%   - winType
-%   - chl.frameF
-%   - chr.frameF
-
-%TODO: doc struct
+%   - frameType: one of the 4 types of frames according to the assignment. From
+%   SSC.
+%   - winType: one of the 2 types of windows. From FILTERBANK.
+%   - chl.frameF: frame of the left channel.
+%   - chr.frameF: frame of the right channel.
+%
+%   See also IAACODER1, SSC, FILTERBANK, DEMOAAC1.
 
 frameWidth = 2048;
 overlap = 0.5;
+winType = 'KBD';
 
 %% Read file
-input = audioread(fNameIn);
+input = audioread(fNameIn); % Assuming 48kHz.
 N = length(input);
 N = N - mod(N, frameWidth); % Number of elements should be divisible by frameWidth.
+input = input(1:N,:);
 
 %% Pad with zeros.
+%TODO: remove or use.
 % Add half a frame before and after.
 % input = [zeros(frameWidth * overlap, 2); input(1:N, :); zeros(frameWidth * overlap, 2)];
 
@@ -27,7 +31,7 @@ AACSeq1 = struct('frameType', {}, 'winType', {}, ...
     'chr', struct('frameF', {}));
 
 %% Perform the encoding
-% numberOfFrames = N / (frameWidth * overlap); % <-- with padding
+% numberOfFrames = N / (frameWidth * overlap); % <-- with padding %TODO: remove or use.
 numberOfFrames = 1 / overlap * (N / frameWidth - 1);
 prevType = 'OLS';
 for frameIdx = 0:numberOfFrames - 1
