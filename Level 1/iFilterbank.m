6d5bbf76321d254cd331310d4f67d601c2fcee10
orestisf <orestisf1993@gmail.com>
2016-12-23 00:53:57 +0200

    Replace mdct function
    Improves performance and SNR now is at 307dB which is the correct value
    according to the instructor.

--- Level 1/iFilterbank.m
+++ Level 1/iFilterbank.m
@@ -48,13 +48,54 @@ assertIsFullFrame(frameT, 'Wrong output size.');
 end
 
 
-function result = imdct(frame)
-% Taken from http://www.ee.columbia.edu/~marios/mdct/imdctv.m.
-M = length(frame);
-N = M * 2;
-n0 = (M + 1) / 2;
+function y = imdct(x)
+% Marios Athineos, marios@ee.columbia.edu
+% http://www.ee.columbia.edu/~marios/
+% Copyright (c) 2002 by Columbia University.
+% All rights reserved.
 
-[k, n] = meshgrid(0:(M - 1), 0:(N - 1));
-T = cos(pi*(n + n0).*(k + 0.5)/M);
-result = T * frame / (N / 4);
+[flen, fnum] = size(x);
+% Make column if it's a single row
+if (flen == 1)
+    x = x(:);
+    flen = fnum;
+    fnum = 1;
+end
+
+% We need these for furmulas below
+N = flen;
+M = N / 2;
+twoN = 2 * N;
+sqrtN = sqrt(twoN);
+
+% We need this twice so keep it around
+t = (0:(M - 1)).';
+w = diag(sparse(exp(-1i*2*pi*(t + 1 / 8)/twoN)));
+
+% Pre-twiddle
+t = (0:(M - 1)).';
+c = x(2*t+1,:) + 1i * x(N-1-2*t+1,:);
+c = (0.5 * w) * c;
+
+% FFT for N/2 points only !!!
+c = fft(c, M);
+
+% Post-twiddle
+c = ((8 / sqrtN) * w) * c;
+
+% Preallocate rotation matrix
+rot = zeros(twoN, fnum);
+
+% Sort
+t = (0:(M - 1)).';
+rot(2*t+1,:) = real(c(t+1,:));
+rot(N+2*t+1,:) = imag(c(t+1,:));
+t = (1:2:(twoN - 1)).';
+rot(t+1,:) = -rot(twoN-1-t+1,:);
+
+% Shift
+t = (0:(3 * M - 1)).';
+y(t+1,:) = rot(t+M+1,:);
+t = (3 * M:(twoN - 1)).';
+y(t+1,:) = - rot(t-3*M+1,:);
 end
