17fc264ac0905970e6da75a8435b501a214571ab
orestisf <orestisf1993@gmail.com>
2017-01-07 02:41:02 +0200

    Move initBands in new file and add qthr

--- Level 2/TNS.m
+++ Level 2/TNS.m
@@ -22,27 +22,6 @@ end
 end
 
 
-function bands = initBands(frameType)
-isESH = strcmp(frameType, 'ESH');
-% Each array ends with the upper limit (w_high) of the last band.
-if isESH
-    % Bands for short frames. B.2.1.9.b.
-    bands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ...
-        16, 17, 19, 21, 23, 25, 27, 29, 31, 34, 37, 40, 43, 46, 50, ...
-        54, 58, 63, 68, 74, 80, 87, 95, 104, 114, 126, 128];
-else
-    % Bands for long frames. B.2.1.9a.
-    bands = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, ...
-        30, 32, 34, 36, 38, 41, 44, 47, 50, 53, 56, 59, 62, 66, 70, ...
-        74, 78, 82, 87, 92, 97, 103, 109, 116, 123, 131, 139, 148, ...
-        158, 168, 179, 191, 204, 218, 233, 249, 266, 284, 304, 325, ...
-        348, 372, 398, 426, 457, 491, 528, 568, 613, 663, 719, 782, ...
-        854, 938, 1024];
-end
-bands = bands + 1;
-end
-
-
 function Sw = normalizationCoeff(X, b)
 %% Calculate energy P and initial Sw.
 Nb = length(b);
