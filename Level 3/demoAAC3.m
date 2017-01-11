a375a3d2dec5f5e93e7f3435bf950f168983841d
orestisf <orestisf1993@gmail.com>
2017-01-11 13:37:11 +0200

    frame->fname in demoAAC3

--- Level 3/demoAAC3.m
+++ Level 3/demoAAC3.m
@@ -1,8 +1,8 @@
-function [SNR, bitrate, compression] = demoAAC3(fNameIn, fNameOut, frameAACoded)
+function [SNR, bitrate, compression] = demoAAC3(fNameIn, fNameOut, fnameAACoded)
 %DEMOAAC3 Demo of the level 3 AAC encoder & decoder.
-%   [SNR, BITRATE, COMPRESSION] = DEMOAAC1(FNAMEIN, FNAMEOUT, FRAMEAACODED) reads the files named
+%   [SNR, BITRATE, COMPRESSION] = DEMOAAC1(FNAMEIN, FNAMEOUT, FNAMEAACODED) reads the files named
 %   FNAMEIN and FNAMEOUT and performs the AAC encoding and decoding. SNR is the signal to noise
 %   ratio of the operation.
 
-[SNR, bitrate, compression] = genericDemo(fNameIn, fNameOut, 3, frameAACoded);
+[SNR, bitrate, compression] = genericDemo(fNameIn, fNameOut, 3, fnameAACoded);
 end
